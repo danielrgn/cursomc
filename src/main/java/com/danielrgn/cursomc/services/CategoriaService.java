b03b13484e0a7dd811aff23bcdcb20b6e361cabe
Daniel Rigoni <danielrgn@hotmail.com>
2018-11-02 17:21:51 -0300

    Adicionando exceptions endpoint categorias

--- src/main/java/com/danielrgn/cursomc/services/CategoriaService.java
+++ src/main/java/com/danielrgn/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.danielrgn.cursomc.domain.Categoria;
 import com.danielrgn.cursomc.repositories.CategoriaRepository;
+import com.danielrgn.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> cat = categoriaRepository.findById(id);
-		return cat.orElse(null);
+		return cat.orElseThrow(() -> 
+				new ObjectNotFoundException("Objeto não encontrado! Id: "+ id + 
+				", Tipo: " + Categoria.class.getName()));
 	}
 }
